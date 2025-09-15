a1d4f9a75641180cde65fdd761304418e8c8e2e4
gradierwerk <gradierwerk.official@gmail.com>
2025-09-15 16:17:21 +0900

    いろいろ

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+function init(){
+    const map = L.map('map', {zoomControl: false}).setView([Math.random() * 75,Math.random() * 360 - 180], 8);
+    L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/earthhillshade/{z}/{x}/{y}.png').addTo(map);
+}
 
+function syncHeight() {
+    const main = document.querySelector("main");
+    const target = document.getElementById("map");
+    target.style.height = main.offsetHeight + "px";
+}
+
+//ロード時
+window.addEventListener("load", () => { init(); syncHeight(); });
+// ウィンドウリサイズ時にも同期
+window.addEventListener("resize", syncHeight);
