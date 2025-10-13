683c10a0df94f2ef11a350fa339e0261712bb67b
hedalu244 <taka0618.m@gmail.com>
2025-10-14 08:19:32 +0900

    Split CSS into files

---
+++ public/_script/code-copy.js
@@ -0,0 +1,12 @@
+document.querySelectorAll(".code-wrapper").forEach(codeBlock => {
+    codeBlock.addEventListener("click", async function () {
+        try {
+            text = this.querySelector("code").textContent;
+            await navigator.clipboard.writeText(text);
+            this.classList.add("clicked");
+            setTimeout(() => this.classList.remove("clicked"), 1500);
+        } catch (err) {
+            console.error("copy failed", err);
+        }
+    })
+});
