a54bbdf61f5f3285e6268182d6f4162749056738
arx-ein <arxein7@gmail.com>
2025-10-14 08:36:02 +0900

    Prettier

--- public/_script/main.js
+++ public/_script/main.js
@@ -1,6 +1,6 @@
-function init(){
-    const map = L.map('map', {zoomControl: false}).setView([Math.random() * 75,Math.random() * 360 - 180], 8);
-    L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/earthhillshade/{z}/{x}/{y}.png').addTo(map);
+function init() {
+  const map = L.map("map", { zoomControl: false }).setView([Math.random() * 75, Math.random() * 360 - 180], 8);
+  L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/earthhillshade/{z}/{x}/{y}.png").addTo(map);
 }
 
 window.addEventListener("load", init);
