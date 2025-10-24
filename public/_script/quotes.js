ddfc8fbac3d89d73f1dfbd977c7beb57904abf51
fabon <syobon.hinata.public@gmail.com>
2025-10-24 21:10:58 +0900

    Refactor `quotes.js`

--- public/_script/quotes.js
+++ public/_script/quotes.js
@@ -1,24 +1,38 @@
-async function getQuote() {
-  try {
-    const quotes = await fetch("/_script/quotes.json").then((res) => res.text(), (e) => { throw(e); });
-    /**
-     * @type {{"author": string, "source": string, "date": string, "content": string}[]}
-     */
-    const quotesJSON = JSON.parse(quotes);
-    const now = new Date().getTime();
-    return quotesJSON[now % quotesJSON.length];
-  } catch (e) {
-    console.error(e);
+/**
+ * @returns {Promise<{author: string, source: string, date: string, content: string}[]>}
+ */
+async function loadQuotes() {
+  // 本当はJSON Modulesを使いたいが、新しい機能ゆえブラウザでのサポートが狭すぎるので`fetch`を使う
+  const response = await fetch("/_script/quotes.json");
+  if (!response.ok) {
+    throw new Error();
   }
+  return response.json();
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const quote = await getQuote();
-  if (!quote) return;
-  const qTag = document.getElementsByName("quote")[0];
-  const authorTag = document.getElementsByName("author")[0];
-  const dateTag = document.getElementsByName("date")[0];
+async function getQuote() {
+  const quotes = await loadQuotes();
+  const now = new Date().getTime();
+  return quotes[now % quotes.length];
+}
+
+/**
+ * @param {{author: string, source: string, date: string, content: string}} quote
+ */
+function showQuote(quote) {
+  const qTag = document.querySelector("[name=quote]");
+  const authorTag = document.querySelector("[name=author]");
+  const dateTag = document.querySelector("[name=date]");
   qTag.textContent = quote.content;
   authorTag.textContent = quote.author;
   dateTag.textContent = quote.date.split("T")[0].replaceAll("-", "/");
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const quote = await getQuote();
+    showQuote(quote);
+  } catch (e) {
+    console.error(e);
+  }
 });
